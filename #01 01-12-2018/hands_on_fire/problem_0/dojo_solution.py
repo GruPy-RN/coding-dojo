a2f03b89d2b2fe7187e9915b48c6f1ca9797f2b9
jotavemonte <jotavemonte@gmail.com>
2018-12-02 16:34:15 -0300

    codigo da comunidade do dojo adicionado

--- #01 01-12-2018/hands_on_fire/problem_0/dojo_solution.py
+++ #01 01-12-2018/hands_on_fire/problem_0/dojo_solution.py
@@ -1,4 +1,21 @@
 def count_users(input_file="input.txt"):
-    with open(input_file) as f:
-        lines = f.readlines()
-    
+	# print('dojo')
+	with open(input_file) as f:
+		lines = f.readlines()
+		
+	emails_normalizados = {
+        normalizar(l) for l in lines[1:]
+    }
+	return len(emails_normalizados)
+        
+
+def normalizar(email):
+	# print("email", email)
+	splited = email.split('@')
+	limpo = splited[0].replace('.', "")
+	# print(limpo)
+	nome_sem_mais = limpo.split('+')[0]
+	print(nome_sem_mais)
+	resultado = nome_sem_mais + "@" + splited[1]
+	print(resultado)
+	return resultado
